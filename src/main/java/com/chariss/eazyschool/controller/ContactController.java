f00bf2c4bee000705c384074c06675460426aa56
Chariss Chomba <chariss.chomba@equitybank.co.ke>
2022-12-01 15:58:01 +0300

    implement holidays page

--- src/main/java/com/chariss/eazyschool/controller/ContactController.java
+++ src/main/java/com/chariss/eazyschool/controller/ContactController.java
@@ -2,8 +2,6 @@ package com.chariss.eazyschool.controller;
 
 import com.chariss.eazyschool.model.Contact;
 import com.chariss.eazyschool.service.ContactService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ContactController {
 
-    private static Logger log = LoggerFactory.getLogger(ContactController.class);
     private final ContactService contactService;
 
     @Autowired
--- src/main/java/com/chariss/eazyschool/service/ContactService.java
+++ src/main/java/com/chariss/eazyschool/service/ContactService.java
@@ -1,18 +1,12 @@
 package com.chariss.eazyschool.service;
 
-import com.chariss.eazyschool.controller.ContactController;
 import com.chariss.eazyschool.model.Contact;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import org.slf4j.LoggerFactory;
-
+@Slf4j
 @Service
 public class ContactService {
-
-    private static Logger log = LoggerFactory.getLogger(ContactController.class);
-
     public boolean saveMessageDetails(Contact  contact) {
         boolean isSaved = true;
         log.info(contact.toString());
