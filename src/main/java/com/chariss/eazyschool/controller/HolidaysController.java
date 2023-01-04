931e3e45aff87d69d69a1df9071622487da77a1d
Chariss Chomba <chariss.chomba@equitybank.co.ke>
2023-01-04 18:27:23 +0300

    Get holidays from database

--- src/main/java/com/chariss/eazyschool/controller/HolidaysController.java
+++ src/main/java/com/chariss/eazyschool/controller/HolidaysController.java
@@ -1,7 +1,9 @@
 package com.chariss.eazyschool.controller;
 
 import com.chariss.eazyschool.model.Holiday;
+import com.chariss.eazyschool.repository.HolidaysRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 public class HolidaysController {
+    @Autowired
+    private HolidaysRepository holidaysRepository;
 
     @GetMapping("/holidays/{display}")
     public String displayHolidays(@PathVariable String display,Model model) {
@@ -26,16 +30,7 @@ public class HolidaysController {
         }else if(null != display && display.equals("festival")){
             model.addAttribute("festival",true);
         }
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidaysRepository.findAllHolidays();
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
