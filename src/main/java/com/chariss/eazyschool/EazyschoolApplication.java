d7fb44b87f32d55795203129c46049d7ae721891
Chariss Chomba <chariss.chomba@equitybank.co.ke>
2023-01-05 16:37:43 +0300

    Add auditing using Spring Data JPA

--- src/main/java/com/chariss/eazyschool/EazyschoolApplication.java
+++ src/main/java/com/chariss/eazyschool/EazyschoolApplication.java
@@ -3,11 +3,13 @@ package com.chariss.eazyschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.chariss.eazyschool.repository")
 @EntityScan("com.chariss.eazyschool.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EazyschoolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/chariss/eazyschool/model/BaseEntity.java
+++ src/main/java/com/chariss/eazyschool/model/BaseEntity.java
@@ -1,17 +1,32 @@
 package com.chariss.eazyschool.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
-
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(updatable = false)
     private String createdBy;
+    @LastModifiedDate
+    @Column(insertable = false)
     private LocalDateTime updatedAt;
+    @LastModifiedBy
+    @Column(insertable = false)
     private String updatedBy;
 }
